5955226114f905c232c0117a30e80e1091c5da30
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-05-04 01:21:24 +0300

    Update 04/05/2017

---
+++ fed-test/scripts/main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	
+	// if ( $(window).width() < 640 ) {
+	// 	$('.mobile-item').hide();
+	// 	$('.list-expander').nextAll().hide();
+	// }
+
+	// $('.menu-opener').click(function() {
+	// 	// e.preventDefault();
+
+	// 	$('.mobile-item').toggleClass('expanded');
+
+	// 	if ( $(window).width() < 640 ) {
+	// 		$('.mobile-item').slideToggle('fast');
+	// 	}
+
+	// 	if ( $(window).width() >= 640 ) {
+	// 		$('.main-menu').toggleClass('visible');
+	// 	}
+
+	// });
+
+	// $('.filter-opener').click(function() {
+
+	// 	$('.left-column').slideToggle('fast');
+
+	// });
+
+	// $(window).resize(function() {
+		
+	// 	if ( !$('.mobile-item').hasClass('expanded') ) {
+	// 		$('.mobile-item').slideUp(0);
+	// 	}
+
+	// 	if ($(window).width() > 640) {
+	// 	    $('.mobile-item').css('display','');
+	// 	    $('.list-expander').nextAll().css('display', '');
+	// 	}
+	// });
+
+	// $('.menu-opener').click(function() {
+	// 	$(this).toggleClass('expanded');
+	// 	$('.menu__wrapper').toggle();
+	// });
+
+});
